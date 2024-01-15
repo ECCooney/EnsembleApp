d8c377e55e51e7067e897a4de9f92af1f648aa4d
Ellen Cooney <elleclarec@gmail.com>
2024-01-15 14:05:19 +0000

    One page CRUD start with Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final db = FirebaseFirestore.instance;
+String? value;
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+      bottomNavigationBar: FloatingActionButton(
+        onPressed: () {
+          // When the User clicks on the button, display a BottomSheet
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return showBottomSheet(context, false, null);
+            },
+          );
+        },
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      appBar: AppBar(
+        title: const Text('Ensemble'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+        // Reading Items form our Database Using the StreamBuilder widget
+        stream: db.collection('items').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data?.docs.length,
+            itemBuilder: (context, int index) {
+              DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
+              return ListTile(
+                title: Text(documentSnapshot['itemName']),
+                onTap: () {
+                  // Here We Will Add The Update Feature and passed the value 'true' to the is update
+                  // feature.
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return showBottomSheet(context, true, documentSnapshot);
+                    },
+                  );
+                },
+                trailing: IconButton(
+                  icon: const Icon(
+                    Icons.delete_outline,
+                  ),
+                  onPressed: () {
+                    // Here We Will Add The Delete Feature
+                    db.collection('items').doc(documentSnapshot.id).delete();
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
+}
+showBottomSheet(
+    BuildContext context, bool isUpdate, DocumentSnapshot? documentSnapshot) {
+  // Added the isUpdate argument to check if our item has been updated
+  return Padding(
+    padding: const EdgeInsets.only(top: 20),
+    child: Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: TextField(
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              // Used a ternary operator to check if isUpdate is true then display
+              // Update Todo.
+              labelText: isUpdate ? 'Update Item' : 'Add Item',
+              hintText: 'Enter An Item',
+            ),
+            onChanged: (String _val) {
+              // Storing the value of the text entered in the variable value.
+              value = _val;
+            },
+          ),
+        ),
+        TextButton(
+            style: ButtonStyle(
+              backgroundColor:
+              MaterialStateProperty.all(Colors.lightBlueAccent),
+            ),
+            onPressed: () {
+              // Check to see if isUpdate is true then update the value else add the value
+              if (isUpdate) {
+                db.collection('items').doc(documentSnapshot?.id).update({
+                  'itemName': value,
+                });
+              } else {
+                db.collection('items').add({'itemName': value});
+              }
+              Navigator.pop(context);
+            },
+            child: isUpdate
+                ? const Text(
+              'UPDATE',
+              style: TextStyle(color: Colors.black),
+            )
+                : const Text('ADD', style: TextStyle(color: Colors.black))),
+      ],
+    ),
+  );
 }
