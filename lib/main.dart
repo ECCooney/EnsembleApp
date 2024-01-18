abe1fb4a69a80700a7e7eedc29558937f59ca2aa
Ellen Cooney <elleclarec@gmail.com>
2024-01-18 11:41:31 +0000

    Firebase set up in project, home screen started

--- lib/main.dart
+++ lib/main.dart
@@ -1,138 +1,42 @@
+import 'package:ensemble/pages/home_page.dart';
+import 'package:ensemble/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+ WidgetsFlutterBinding.ensureInitialized(); //ensure all Widgets are initilised
+ if (kIsWeb){
+   //run the initialisation for web
+   await Firebase.initializeApp(
+       options: FirebaseOptions
+         (apiKey: Constants.apiKey,
+          appId: Constants.appId,
+          messagingSenderId: Constants.messagingSenderId,
+          projectId: Constants.projectId));
+ }
+ else{
+   //run intialization for android/iOs
+   await Firebase.initializeApp(); //initialises Anroid and IoS if no options provided
+ }
+
+ runApp(const MyApp());
+
 }
 
 final db = FirebaseFirestore.instance;
 String? value;
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key); // takes an optional Key parameter, which can be used to uniquely identify this widget. The super(key: key) part calls the constructor of the superclass (StatefulWidget).
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
-}
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: FloatingActionButton(
-        onPressed: () {
-          // When the User clicks on the button, display a BottomSheet
-          showModalBottomSheet(
-            context: context,
-            builder: (context) {
-              return showBottomSheet(context, false, null);
-            },
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        title: const Text('Ensemble'),
-        centerTitle: true,
-      ),
-      body: StreamBuilder(
-        // Reading Items form our Database Using the StreamBuilder widget
-        stream: db.collection('items').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView.builder(
-            itemCount: snapshot.data?.docs.length,
-            itemBuilder: (context, int index) {
-              DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
-              return ListTile(
-                title: Text(documentSnapshot['itemName']),
-                onTap: () {
-                  // Here We Will Add The Update Feature and passed the value 'true' to the is update
-                  // feature.
-                  showModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return showBottomSheet(context, true, documentSnapshot);
-                    },
-                  );
-                },
-                trailing: IconButton(
-                  icon: const Icon(
-                    Icons.delete_outline,
-                  ),
-                  onPressed: () {
-                    // Here We Will Add The Delete Feature
-                    db.collection('items').doc(documentSnapshot.id).delete();
-                  },
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-showBottomSheet(
-    BuildContext context, bool isUpdate, DocumentSnapshot? documentSnapshot) {
-  // Added the isUpdate argument to check if our item has been updated
-  return Padding(
-    padding: const EdgeInsets.only(top: 20),
-    child: Column(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.9,
-          child: TextField(
-            decoration: InputDecoration(
-              border: const OutlineInputBorder(),
-              // Used a ternary operator to check if isUpdate is true then display
-              // Update Todo.
-              labelText: isUpdate ? 'Update Item' : 'Add Item',
-              hintText: 'Enter An Item',
-            ),
-            onChanged: (String val) {
-              // Storing the value of the text entered in the variable value.
-              value = val;
-            },
-          ),
-        ),
-        TextButton(
-            style: ButtonStyle(
-              backgroundColor:
-              MaterialStateProperty.all(Colors.lightBlueAccent),
-            ),
-            onPressed: () {
-              // Check to see if isUpdate is true then update the value else add the value
-              if (isUpdate) {
-                db.collection('items').doc(documentSnapshot?.id).update({
-                  'itemName': value,
-                });
-              } else {
-                db.collection('items').add({'itemName': value});
-              }
-              Navigator.pop(context);
-            },
-            child: isUpdate
-                ? const Text(
-              'UPDATE',
-              style: TextStyle(color: Colors.black),
-            )
-                : const Text('ADD', style: TextStyle(color: Colors.black))),
-      ],
-    ),
-  );
 }
---
+++ lib/shared/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static String appId = "1:537569368518:web:a3fcfef8050de777a6bac7";
+  static String apiKey = "AIzaSyCZ686lYWvDoDWpcKrecDd3A48_1o7RevU";
+  static String messagingSenderId = "537569368518";
+  static String projectId = "ensemble-dbb5d";
+
+}
