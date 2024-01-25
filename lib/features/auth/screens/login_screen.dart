e992818b5dfe7241c5d2aacecb2afe6b4a85d05f
Ellen Cooney <elleclarec@gmail.com>
2024-01-25 12:23:38 +0000

    Refaactoring with riverpod and error handling - Google sign in started

---
+++ lib/features/auth/screens/login_screen.dart
@@ -0,0 +1,115 @@
+import 'package:ensemble/core/common/sign_in_button.dart';
+import 'package:flutter/material.dart';
+
+import '../../../core/constants/constants.dart';
+import '../../../theme/pallete.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey();
+    bool _isLoading = false;
+    String email = "";
+    String password = "";
+    return Scaffold(
+        appBar: AppBar(
+          title: Image.asset(Constants.initialLogoPath,height: 40,),
+          actions: [
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'About',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+          backgroundColor: Pallete.orangeCustomColor,
+        ),
+        body: _isLoading ? Center(child: CircularProgressIndicator(color: Theme.of(context).primaryColor),):
+        SingleChildScrollView( //creates scroll bar if screen overflows
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget> [
+                    const Text(
+                        "Share and Share Alike",
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                    ),
+                    const SizedBox(height: 30),
+                    Image.asset(Constants.logoPath),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.email),
+                        hintText: 'Enter the email you registered with',
+                        labelText: 'Email',
+                      ),
+                      //validate whether it's an acceoptable email
+                      validator: (val) {
+                        return RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(val!)
+                            ? null //return nothing if that value matchs the pattern
+                            : "Please enter a valid email";
+                      },
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.lock),
+                        hintText: 'Password',
+                        labelText: 'Password',
+                      ),
+                        validator: (val){
+                          if (val!.length < 6) {
+                            return "Password must be at least 6 characters";
+                          } else {
+                            return null;
+                          }
+                        },
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width:double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Pallete.sageCustomColor,
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))
+                        ),
+                        child: const Text("Sign In", style: TextStyle(color: Colors.white, fontSize: 16)
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text.rich(//can create two text children in one text piece using Text.rich
+                        TextSpan(
+                            text: "Don't have an account? ",
+                            style: TextStyle(color: Colors.black, fontSize: 14),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: "Register Here",
+                                  style: TextStyle(color: Colors.black, decoration: TextDecoration.underline),
+                              )
+                            ]
+                        )
+                    ),
+              const SignInButton(),
+                  ],
+                )
+            ),
+
+          ),
+        )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,34 @@
-import 'package:ensemble/pages/auth/login_page.dart';
-import 'package:ensemble/helper/helper_function.dart';
-import 'package:ensemble/pages/home_page.dart';
-import 'package:ensemble/shared/constants.dart';
+import 'package:ensemble/features/auth/screens/login_screen.dart';
+import 'package:ensemble/theme/pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
- WidgetsFlutterBinding.ensureInitialized(); //ensure all Widgets are initilised
- if (kIsWeb){
-   //run the initialisation for web
-   await Firebase.initializeApp(
-       options: FirebaseOptions
-         (apiKey: Constants.apiKey,
-          appId: Constants.appId,
-          messagingSenderId: Constants.messagingSenderId,
-          projectId: Constants.projectId));
- }
- else{
-   //run intialization for android/iOs
-   await Firebase.initializeApp(); //initialises Anroid and IoS if no options provided
- }
-
- runApp(const MyApp());
-
+  WidgetsFlutterBinding. ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+  );
 }
 
-final db = FirebaseFirestore.instance;
-String? value;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // This widget is the root of your application.
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  bool _isSignedIn = false; //underscore in dart means the varible is private to it's library
- // takes an optional Key parameter, which can be used to uniquely identify this widget. The super(key: key) part calls the constructor of the superclass (StatefulWidget).
-  @override
-  void initState() {
-    super.initState();
-    getUserLoggedInStatus();
-  }
-
-  getUserLoggedInStatus() async {
-    await HelperFunctions.getUserLoggedInStatus().then((value){
-      if(value!=null){
-        setState(() {
-          _isSignedIn = value;
-        });
-      }
-    });
-  }
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Constants().primaryColor,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      debugShowCheckedModeBanner: false,
-      //if isSignedIn = True show Homepage, else show LoginPage
-      home: _isSignedIn ? const HomePage() : const LoginPage(),
+      title: 'Flutter Demo',
+      theme: Pallete.lightModeAppTheme,
+      home: const LoginScreen(),
     );
   }
 }
+
